49898e581343d8525b4b49ade16040618839c1e1
dragonette13 <65913060+dragonette13@users.noreply.github.com>
2024-05-09 11:03:45 -0700

    added card class and deck array

---
+++ src/Card.java
@@ -0,0 +1,33 @@
+import processing.core.PImage;
+public class Card {
+    private int num;
+    private Suit suit;
+    private PImage img;
+    private int WIDTH, HEIGHT;
+    private String imageName;
+
+    public Card(int num, Suit suit){
+        this.num = num;
+        this.suit = suit;
+        WIDTH = 20;
+        HEIGHT = 30;
+        // this.img = img;
+        imageName = num+"_of_";
+        if(suit == Suit.HEARTS){
+           imageName+="hearts.png";
+        }else if(suit == Suit.DIAMONDS){
+            imageName+="diamonds.png";
+        }else if(suit == Suit.CLUBS){
+            imageName+="clubs.png";
+        }else if(suit == Suit.SPADES){
+            imageName+="spades.png";
+        }
+        img = Main.app.loadImage(imageName);
+
+
+    }
+
+    public void display(int x, int y){
+        Main.app.image(img, x, y, x+WIDTH, x+HEIGHT);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import processing.core.PApplet;
-
+import processing.core.PImage;
+import java.util.*;
 public class Main extends PApplet {
 
-    private int zoom;
-    private float weight;
+    private ArrayList<Card> deck;
     //setting up PApplet
     public static Main app;
     public static void main(String[] args) {
@@ -21,6 +21,24 @@ public class Main extends PApplet {
 
     //init
     public void setup() {
+        deck = new ArrayList<Card>();
+        Suit currentSuit;
+
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=13; j++){
+                if(i == 1){
+                    currentSuit = Suit.HEARTS;
+                }else if(i == 2){
+                    currentSuit = Suit.DIAMONDS;
+                }else if(i == 3){
+                    currentSuit = Suit.CLUBS;
+                }else{
+                    currentSuit = Suit.SPADES;
+                }
+                deck.add(new Card(j, currentSuit));
+            }
+        }
+
 
     }
 
