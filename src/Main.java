cb71486210e6894dec4878dd177769abfbe5eee8
katelynli <26kli@castilleja.org>
2024-05-13 09:46:37 -0700

    board is clickable. need to make the other pile like actually exist instead of it being empty as soon as you place a card

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,14 @@ public class Main extends PApplet {
     private int deckX, deckY, currCardX, currCardY, pilesX, heartsY, diamondsY, clubsY, spadesY, boardX, boardY;
     // int heartsPile, diamondsPile, clubsPile, spadesPile;
     private Card currHearts, currDiamonds, currClubs, currSpades;
-    public static int  CARD_WIDTH, CARD_HEIGHT;
+    public static int CARD_WIDTH, CARD_HEIGHT;
     private int currCardIndex;
     private Card currCard;
     private Card[][] board;
 
     //setting up PApplet
     public static Main app;
+
     public static void main(String[] args) {
         PApplet.main("Main");
     }
@@ -36,28 +37,28 @@ public class Main extends PApplet {
         CARD_HEIGHT = 130;
         deckX = 20;
         deckY = 20;
-        boardX = deckX + CARD_WIDTH + CARD_WIDTH/2;
+        boardX = deckX + CARD_WIDTH + CARD_WIDTH / 2;
         boardY = deckY;
         currCardX = deckX;
-        currCardY = deckY + CARD_WIDTH/2 + CARD_HEIGHT;
+        currCardY = deckY + CARD_WIDTH / 2 + CARD_HEIGHT;
         currCardIndex = -1;
-        pilesX = width - CARD_WIDTH*2;
-        heartsY = CARD_WIDTH/2;
-        diamondsY = heartsY + CARD_HEIGHT + CARD_WIDTH/2;
-        clubsY = diamondsY + CARD_HEIGHT + CARD_WIDTH/2;
-        spadesY = clubsY + CARD_HEIGHT + CARD_WIDTH/2;
+        pilesX = width - CARD_WIDTH * 2;
+        heartsY = CARD_WIDTH / 2;
+        diamondsY = heartsY + CARD_HEIGHT + CARD_WIDTH / 2;
+        clubsY = diamondsY + CARD_HEIGHT + CARD_WIDTH / 2;
+        spadesY = clubsY + CARD_HEIGHT + CARD_WIDTH / 2;
         deck = new ArrayList<Card>();
         board = new Card[13][7];
         Suit currentSuit;
-        for(int i=1; i<=4; i++){
-            for(int j=1; j<=13; j++){
-                if(i == 1){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 13; j++) {
+                if (i == 1) {
                     currentSuit = Suit.HEARTS;
-                }else if(i == 2){
+                } else if (i == 2) {
                     currentSuit = Suit.DIAMONDS;
-                }else if(i == 3){
+                } else if (i == 3) {
                     currentSuit = Suit.CLUBS;
-                }else{
+                } else {
                     currentSuit = Suit.SPADES;
                 }
                 deck.add(new Card(j, currentSuit));
@@ -78,99 +79,109 @@ public class Main extends PApplet {
     public void draw() {
         background(200);
         image(loadImage("url.jpeg"), deckX, deckY, CARD_WIDTH, CARD_HEIGHT);
-        if(currCard != null) {
+        if (currCard != null) {
             currCard.display(currCardX, currCardY);
         }
-        if(currHearts != null){
+        if (currHearts != null) {
             currHearts.display(pilesX, heartsY);
         }
-        if(currDiamonds != null){
+        if (currDiamonds != null) {
             currDiamonds.display(pilesX, diamondsY);
         }
-        if(currClubs != null){
+        if (currClubs != null) {
             currClubs.display(pilesX, clubsY);
         }
-        if(currSpades != null){
+        if (currSpades != null) {
             currSpades.display(pilesX, spadesY);
         }
         for (int c = 0; c < board[0].length; c++) {
             for (int r = 0; r < board.length; r++) {
                 if (board[r][c] != null) {
-                    board[r][c].display(boardX + c * (CARD_WIDTH + CARD_WIDTH/2), boardY + r * CARD_WIDTH/2);
+                    board[r][c].display(boardX + c * (CARD_WIDTH + CARD_WIDTH / 2), boardY + r * CARD_WIDTH / 2);
                 }
             }
         }
     }
 
-    public void mouseClicked(){
-       // if(mouseX>deckX && mouseX<deckX+CARD_WIDTH && mouseY>deckY && mouseY<deckY+CARD_HEIGHT){
-        if(checkMouseBounds(deckX, deckY, CARD_WIDTH, CARD_HEIGHT)){
-            currCardIndex ++;
-            if(currCardIndex>=deck.size()){
+    public void mouseClicked() {
+        // if(mouseX>deckX && mouseX<deckX+CARD_WIDTH && mouseY>deckY && mouseY<deckY+CARD_HEIGHT){
+        int r;
+        for (int c = 0; c < board[0].length; c++) {
+            r = getLast(c);
+            if (checkMouseBounds(boardX + c * (CARD_WIDTH + CARD_WIDTH / 2), boardY + r * CARD_WIDTH / 2, CARD_WIDTH, CARD_HEIGHT)) {
+                addToPile(board[r][c]);
+                board[r][c] = null;
+                if (r > 0) {
+                    board[r - 1][c].flip();
+                }
+            }
+        }
+        if (checkMouseBounds(deckX, deckY, CARD_WIDTH, CARD_HEIGHT)) {
+            currCardIndex++;
+            if (currCardIndex >= deck.size()) {
                 currCardIndex = -1;
                 currCard = null;
-            }else{
+            } else {
                 currCard = deck.get(currCardIndex);
             }
 
-        }else if(currCard!=null && checkMouseBounds(currCardX, currCardY, CARD_WIDTH, CARD_HEIGHT)){
-//            if(currCard.getSuit() == Suit.HEARTS){
-//                if(currCard.getNum()==1) {
-//                    currHearts = currCard;
-//                    deck.remove(currHearts);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }else if(currHearts!= null && currCard.getNum()==currHearts.getNum()+1){
-//                    currHearts = currCard;
-//                    deck.remove(currHearts);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }
-//            }else if(currCard.getSuit() == Suit.DIAMONDS){
-//                if(currCard.getNum()==1) {
-//                    currDiamonds = currCard;
-//                    deck.remove(currDiamonds);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }else if(currDiamonds!= null && currCard.getNum()==currDiamonds.getNum()+1){
-//                    currDiamonds = currCard;
-//                    deck.remove(currDiamonds);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }
-//            }else if(currCard.getSuit() == Suit.CLUBS){
-//                if(currCard.getNum()==1) {
-//                    currClubs = currCard;
-//                    deck.remove(currClubs);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }else if(currClubs!= null && currCard.getNum()==currClubs.getNum()+1){
-//                    currClubs = currCard;
-//                    deck.remove(currClubs);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }
-//            }else if(currCard.getSuit() == Suit.SPADES){
-//                if(currCard.getNum()==1) {
-//                    currSpades = currCard;
-//                    deck.remove(currSpades);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }else if(currSpades!= null && currCard.getNum()==currSpades.getNum()+1){
-//                    currSpades = currCard;
-//                    deck.remove(currSpades);
-//                    currCardIndex--;
-//                    currCard = null;
-//                }
-            currCard.flip();
+        }else if (currCard != null && checkMouseBounds(currCardX, currCardY, CARD_WIDTH, CARD_HEIGHT)) {
+            addToPile(currCard);
+            currCardIndex--;
+            currCard = null;
+        }
+    }
+
+    public void keyPressed(){
+
+    }
+
+    private boolean checkMouseBounds (int x, int y, int w, int h){
+        return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
+    }
+
+    private int getLast(int col) {
+        for (int r = 0; r < board.length - 1; r++) {
+            if (board[r + 1][col] == null) {
+                return r;
             }
         }
-
-    private boolean checkMouseBounds(int x, int y, int w, int h){
-        return mouseX>x && mouseX<x+w && mouseY>y && mouseY<y+h;
+        return board.length - 1;
     }
-//    public void keyPressed(){
-//
-//    }
 
+    private void addToPile(Card card) {
+        if (card.getSuit() == Suit.HEARTS) {
+            if (card.getNum() == 1) {
+                currHearts = card;
+                deck.remove(currHearts);
+            } else if (currHearts != null && card.getNum() == currHearts.getNum() + 1) {
+                currHearts = card;
+                deck.remove(currHearts);
+            }
+        } else if (card.getSuit() == Suit.DIAMONDS) {
+            if (card.getNum() == 1) {
+                currDiamonds = card;
+                deck.remove(currDiamonds);
+            } else if (currDiamonds != null && card.getNum() == currDiamonds.getNum() + 1) {
+                currDiamonds = card;
+                deck.remove(currDiamonds);
+            }
+        } else if (card.getSuit() == Suit.CLUBS) {
+            if (card.getNum() == 1) {
+                currClubs = card;
+                deck.remove(currClubs);
+            } else if (currClubs != null && card.getNum() == currClubs.getNum() + 1) {
+                currClubs = card;
+                deck.remove(currClubs);
+            }
+        } else if (card.getSuit() == Suit.SPADES) {
+            if (card.getNum() == 1) {
+                currSpades = card;
+                deck.remove(currSpades);
+            } else if (currSpades != null && card.getNum() == currSpades.getNum() + 1) {
+                currSpades = card;
+                deck.remove(currSpades);
+            }
+        }
+    }
 }
