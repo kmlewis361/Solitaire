5b2ed18527cecf98bf88bac2b9c0f2367ac8673b
dragonette13 <65913060+dragonette13@users.noreply.github.com>
2024-05-20 10:54:48 -0700

    initialize currHearts etc

--- src/Main.java
+++ src/Main.java
@@ -49,6 +49,10 @@ public class Main extends PApplet {
         drawnPile = new ArrayList<Card>();
         board = new Card[13][7];
         gameDone = false;
+        currHearts = null;
+        currDiamonds = null;
+        currClubs = null;
+        currSpades = null;
         Suit currentSuit;
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= 13; j++) {
